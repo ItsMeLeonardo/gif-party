d2bd9a1a9d4bece692c6a6e9dc571f1a7cdb2c37
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-11-02 18:36:15 -0500

    add linter to project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-console": "warn",
+    "import/prefer-default-export": 0,
+    "react/react-in-jsx-scope": 0,
+    "import/no-unresolved": 0,
+    "no-unused-vars": "warn",
+    "react/prop-types": 0,
+  },
+};
